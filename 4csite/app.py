c09fb8681530649f338d606fc76cbe91441ae62c
4cerifolia <603898428@qq.com>
2023-07-07 16:53:44 +0800

    myproject-firstday

--- 4csite/app.py
+++ 4csite/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def phone_list():
     return render_template('phone_list.html')
 
+@app.route('/')
+def homepage():
+    return render_template('homepage')
+
 
 if __name__=='__main__':
     app.run()
--- 4csite/static/read.md
+++ 4csite/static/read.md
@@ -1,1 +1,4 @@
 本文件夹放静态文件：图片，下载的bootstrap
+
+1bootstrap
+2jquery
